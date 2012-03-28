02492bfa926b28a5f529dee338bbc3f64ae31033
Ben Ward <benward@twitter.com>
2012-03-28 11:36:18 -0700

    Tweaks for office machine.

--- bash/loader.bash
+++ bash/loader.bash
@@ -2,5 +2,7 @@ source ~/etc/bash/paths.bash
 source ~/etc/bash/machine.bash
 source ~/etc/bash/profile.bash
 source ~/etc/bash/functions.bash
+source ~/etc/bash/node.bash
+source ~/etc/bash/ruby.bash
 source ~/etc/bash/sourcecontrol.bash
 source ~/etc/bash/macosx.bash
