7b7153ecda786c51e5634963c232a62c2610175b
Ben Ward <benward@twitter.com>
2015-05-27 16:57:06 -0700

    Add fuck/kinit-git/pants support

---
+++ bash/git.bash
@@ -0,0 +1,1 @@
+alias git="klist -s || kinit; git"
--- bash/loader.bash
+++ bash/loader.bash
@@ -1,9 +1,12 @@
 source ~/etc/bash/paths.bash
 source ~/etc/bash/applications.bash
+source ~/etc/bash/fuck.bash
 source ~/etc/bash/machine.bash
 source ~/etc/bash/profile.bash
 source ~/etc/bash/functions.bash
+source ~/etc/bash/git.bash
 source ~/etc/bash/node.bash
+source ~/etc/bash/pants.bash
 source ~/etc/bash/ruby.bash
 source ~/etc/bash/sourcecontrol.bash
 source ~/etc/bash/macosx.bash
