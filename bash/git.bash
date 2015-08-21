603ef4fef9366aeb57b9532c726f65a881e0673a
Ben Ward <benward@twitter.com>
2015-08-20 21:33:35 -0700

    Move sourcecontrol.bash into git.bash:

--- bash/git.bash
+++ bash/git.bash
@@ -1,1 +1,5 @@
-alias git="klist -s || kinit; git"
+sourceif /usr/local/etc/bash_completion.d/git-completion.bash
+sourceif /usr/local/etc/bash_completion.d/git-prompt.sh
+
+alias github="git remote -v | ruby -e 'slug = STDIN.read.split(\"\n\").collect { |line| /git@github\.com:([\w\/\.]+)/.match(line) ? \$1 : false }.select { |line| line }.first; \`open https://github.com/#{slug}\`'"
+# alias git="klist -s || kinit; git"
