bd8d3c72fa206f550002991d037dc4d2edcb1d8a
Ben Ward <benward@twitter.com>
2015-08-20 21:39:01 -0700

    Put Twitter kinit git alias behind an if

--- bash/git.bash
+++ bash/git.bash
@@ -2,4 +2,5 @@ sourceif $BREWDIR/etc/bash_completion.d/git-completion.bash
 sourceif $BREWDIR/etc/bash_completion.d/git-prompt.sh
 
 alias github="git remote -v | ruby -e 'slug = STDIN.read.split(\"\n\").collect { |line| /git@github\.com:([\w\/\.]+)/.match(line) ? \$1 : false }.select { |line| line }.first; \`open https://github.com/#{slug}\`'"
-# alias git="klist -s || kinit; git"
+
+[[ -e /opt/twitter ]] && alias git="klist -s || kinit; git"
