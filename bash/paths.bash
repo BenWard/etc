dd2ce80b6d6ae862e1acd2efac50fe1233707544
Ben Ward <benward@twitter.com>
2015-08-20 21:42:11 -0700

    /Users/benward in paths

--- bash/paths.bash
+++ bash/paths.bash
@@ -11,16 +11,16 @@ export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 
 ## Ruby
 export PATH=/.rvm/bin:$PATH
-export PATH=/usr/local/Cellar/ruby/1.9.3-p0/bin:$PATH
+export PATH=$BREWDIR/Cellar/ruby/1.9.3-p0/bin:$PATH
 
 ## MySQL
 export PATH=/usr/local/mysql/bin:$PATH
 
 ## Twitter
 export PATH=/opt/twitter/bin:/opt/twitter/sbin:$PATH
-export PATH=/Users/benward/.twitools/src/twitter-utilities/sbt:/Users/benward/.twitools/src/twitter-utilities/sbt11:/Users/benward/.twitools/src/twitter-utilities/bin:$PATH
-export PATH=~/code/twitter-utilities:$PATH
+export PATH=$HOME/.twitools/src/twitter-utilities/sbt:$HOME/.twitools/src/twitter-utilities/sbt11:$HOME/.twitools/src/twitter-utilities/bin:$PATH
+export PATH=$HOME/code/twitter-utilities:$PATH
 
 ## Node
-export PATH=~/local/node/bin:$PATH
+export PATH=$HOME/local/node/bin:$PATH
 export PATH=/opt/twitter/share/npm/bin/:$PATH
