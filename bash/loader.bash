cf569bfaa5c184be7bc4f9be28bf077777e96624
Ben Ward <ben@benward.uk>
2018-02-20 18:20:45 -0800

    Fix path errors in loader

--- bash/loader.bash
+++ bash/loader.bash
@@ -1,22 +1,24 @@
+DOTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Use https://github.com/mrzool/bash-sensible as a base:
-source ./sensible/sensible.bash
+source $DOTPATH/sensible/sensible.bash
 
-source ./terminal.bash
-source machine.bash
+source $DOTPATH/terminal.bash
+source $DOTPATH/machine.bash
 
-source ./paths.bash
-source ./completions.bash
-source ./functions.bash
+source $DOTPATH/paths.bash
+source $DOTPATH/completions.bash
+source $DOTPATH/functions.bash
 
-source ./profile.bash
+source $DOTPATH/profile.bash
 
-source ./applications.bash
-source ./fuck.bash
-source ./git.bash
-source ./pants.bash
+source $DOTPATH/applications.bash
+source $DOTPATH/fuck.bash
+source $DOTPATH/git.bash
+source $DOTPATH/pants.bash
 
-source ./node.bash
-source ./ruby.bash
-source ./scala.bash
+source $DOTPATH/node.bash
+source $DOTPATH/ruby.bash
+source $DOTPATH/scala.bash
 
 sourceif ~/.extras.bash
