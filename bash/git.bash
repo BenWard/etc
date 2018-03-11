be30e61c89932b88dc45b790b0beec2f201cc09c
Ben Ward <ben@benward.uk>
2018-03-11 03:32:23 -0700

    Start adding some CentOS path variants

--- bash/git.bash
+++ bash/git.bash
@@ -1,4 +1,8 @@
+# Mac
 sourceif $BREWDIR/etc/bash_completion.d/git-completion.bash
 sourceif $BREWDIR/etc/bash_completion.d/git-prompt.sh
+
+# CentOS
+sourceif /usr/share/git-core/contrib/completion/git-prompt.sh
 
 alias github="git remote -v | ruby -e 'slug = STDIN.read.split(\"\n\").collect { |line| /git@github\.com:([\w\/\.]+)/.match(line) ? \$1 : false }.select { |line| line }.first; \`open https://github.com/#{slug}\`'"
