cb193b50dd7dbdd3df2852505149261ca173e7db
Ben Ward <ben@benward.uk>
2018-03-12 16:33:51 -0700

    Match - in git repo names

--- bash/git.bash
+++ bash/git.bash
@@ -5,4 +5,4 @@ sourceif $BREWDIR/etc/bash_completion.d/git-prompt.sh
 # CentOS
 sourceif /usr/share/git-core/contrib/completion/git-prompt.sh
 
-alias github="git remote -v | ruby -e 'slug = STDIN.read.split(\"\n\").collect { |line| /git@github\.com:([\w\/\.]+)/.match(line) ? \$1 : false }.select { |line| line }.first; \`open https://github.com/#{slug}\`'"
+alias github="git remote -v | ruby -e 'slug = STDIN.read.split(\"\n\").collect { |line| /git@github\.com:([\w\/\.\-_]+)/.match(line) ? \$1 : false }.select { |line| line }.first; \`open https://github.com/#{slug}\`'"
