9a231906200ddab3ced5524e8c25341fd954ae8f
Ben Ward <ben@benward.uk>
2026-05-19 14:27:44 -0700

    Move git aliases to scripts for easier reading; add default claude.md

--- bash/git.bash
+++ bash/git.bash
@@ -6,3 +6,12 @@ sourceif $BREWDIR/etc/bash_completion.d/git-prompt.sh
 sourceif /usr/share/git-core/contrib/completion/git-prompt.sh
 
 alias github="git remote -v | ruby -e 'slug = STDIN.read.split(\"\n\").collect { |line| /git@github\.com:([\w\/\.\-_]+)/.match(line) ? \$1 : false }.select { |line| line }.first; \`open https://github.com/#{slug}\`'"
+
+gitroot() {
+  local root="$(git rev-parse --show-toplevel 2>/dev/null)"
+  if [ -z "$root" ]; then
+    echo "Not a git repository" >&2
+    return 1
+  fi
+  cd "$root/$1"
+}
